77616cab18f431930e4139383f5c73888ece28a6
Mohamed Ben Henda <benhenda.med.tn@gmail.com>
2018-01-16 11:14:41 +0100

    ymwImap Bundle class

---
+++ imap-bundle/YmwImapBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Project: imap
+ * User: m.benhenda
+ * Date: 16/01/2018
+ * Time: 11:12
+ */
+namespace YMW\ImapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YmwImapBundle extends Bundle
+{
+
+}
